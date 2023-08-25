b4d91c4465300410c729b871e78749a65c638c44
Hashaam Zafar <hashaam1217@gmail.com>
2023-08-25 06:18:23 +0500

    Made a 'list' view and updated models

--- user_flagging/flagging/models.py
+++ user_flagging/flagging/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+#from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    email = models.EmailField(blank=True)
+    phone_number = models.CharField(max_length=11)
+    
+    def __str__(self):
+        return f"{self.id} {self.first_name} {self.last_name}"
+
+class Business(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.id}: {self.name}"
+
+class Flag(models.Model): 
+    Date = models.DateField()
+    Message = models.CharField(max_length=200, blank=True)
+    Customer = models.ManyToManyField("Customer", related_name="flags")
+    businesses = models.ManyToManyField("Business", related_name="users")
+
+    def __str__(self):
+        return f"{self.id}: {self.Date}"
--- user_flagging/flagging/views.py
+++ user_flagging/flagging/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-
+from .models import Customer, Business, Flag 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, "flagging/index.html")
+
+def list(request):
+    return render(request, "flagging/list.html", {
+        "Customer": Customer.objects.all(),
+        }) 
