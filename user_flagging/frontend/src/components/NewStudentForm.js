7429a78a9bbb4d9620691e39e5c48a3474b0fdc8
Hashaam Zafar <hashaam1217@gmail.com>
2023-08-29 03:00:39 +0500

    Working on NewCustomer

--- user_flagging/frontend/src/components/NewStudentForm.js
+++ user_flagging/frontend/src/components/NewStudentForm.js
@@ -14,5 +14,30 @@ class NewCustomerForm extends React.Component
             last_name: "" ,
             email: "", 
             phone: "",
+            flags: [],  
+        };
+
+    componentDidMount()
+    {
+        if (this.props.customer)
+        {
+            const {id, first_name, last_name, email, phone, flags} = this,props.customer;
+            this.setState({id, first_name, last_name, email, phone, flags});
         }
+    }
+
+    onChange = e =>
+    {
+        this.setState({ [e.target.name]: e.target.value })
+    };
+
+    createStudent = e =>
+    {
+        e.preventDefault();
+        axios.post(API_URL, this.state).then(() =>
+            {
+                this.props.resetState();
+                this.props.toggle();
+            });
+    };
 }
