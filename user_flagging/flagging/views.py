3c00b23159ef7122e414680948af6daf5837e2f6
Hashaam Zafar <hashaam1217@gmail.com>
2023-08-28 16:20:16 +0500

    Fixed Customer information page

--- user_flagging/flagging/views.py
+++ user_flagging/flagging/views.py
@@ -9,3 +9,16 @@ def list(request):
     return render(request, "flagging/list.html", {
         "Customer": Customer.objects.all(),
         }) 
+
+def customer(request, customer_id): 
+    Current_Customer = Customer.objects.get(id=customer_id)
+    flags = []
+    for count in range(Current_Customer.flags.count()):
+        current_flag = Current_Customer.flags.get(id=count+1)
+        flags.append(current_flag)
+        
+    return render(request, "flagging/customer.html", {
+        "customer_id":customer_id,
+        "Customer": Current_Customer,
+        "flags":flags
+        })
