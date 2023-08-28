4ea2ee1e5eceb0db5eb093b59550bfd0a1bd614c
Hashaam Zafar <hashaam1217@gmail.com>
2023-08-28 21:42:17 +0500

    Introduced home page

--- user_flagging/user_flagging/urls.py
+++ user_flagging/user_flagging/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     # Google Auth
-    path("", TemplateView.as_view(template_name="index.html")),
+    path("", TemplateView.as_view(template_name="user_flagging/index.html")),
     path("accounts/", include("allauth.urls")),
     path("logout", LogoutView.as_view()),
 
